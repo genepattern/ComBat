0c96b88be8d889ef002a40af687602131b516f3f
nazaire <nazaire@47fccdf2-e235-0410-87e5-63258e95d6e5>
2008-05-02 03:31:01 +0000

    added code to handle res file
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/COMBAT@13763 47fccdf2-e235-0410-87e5-63258e95d6e5

--- gp.combat.R
+++ gp.combat.R
@@ -39,7 +39,7 @@ parseCmdLine <- function(...) {
    		}
    		else if(flag=='-f')
 		{
-			filter <- value
+			filter <- as.numeric(value)
    		}
    		else if(flag=='-p')
 		{
@@ -64,11 +64,11 @@ parseCmdLine <- function(...) {
 		}
     }
 
-    gp.combat.R(input.file.name, sample.info.file.name, libdir, output.file.name, prior.plots, par.prior)
+    gp.combat.R(input.file.name = input.file.name, sample.info.file.name =sample.info.file.name, libdir = libdir, output.file.name = output.file.name, prior.plots = prior.plots , par.prior = par.prior, filter = filter)
 
 }
 
-gp.combat.R <- function(input.file.name, sample.info.file.name, libdir, output.file.name, prior.plots, par.prior)
+gp.combat.R <- function(input.file.name, sample.info.file.name, libdir, output.file.name, prior.plots, par.prior, filter)
 {
     setup(libdir)
 
@@ -78,31 +78,65 @@ gp.combat.R <- function(input.file.name, sample.info.file.name, libdir, output.f
     combat.input.file.name <- "combat_input_file.txt"
     on.exit(unlink(combat.input.file.name))
 
-    #check if it is a gct file
-    if(is.null(dataset$calls))
+    column.names <- c("ProbeID", colnames(data.matrix))
+    #check if it is a res file
+    if(!is.null(dataset$calls))
     {
-        column.names <- c("ProbeID", colnames(data.matrix))
-        cat(column.names, file=combat.input.file.name, sep="\t")
-        cat("\n", file = combat.input.file.name, append=TRUE)
-        write.table(data.matrix, file=combat.input.file.name, sep="\t", col.names = FALSE, quote=FALSE, append=TRUE)
+        column.names <- NULL
+        input.table <- NULL
+        for(i in 1:ncol(data.matrix))
+        {
+            col.name <- colnames(data.matrix)[i]
+            calls.column.name <- paste(col.name, "call", sep=' ')
+            column.names <- c(column.names, col.name, calls.column.name)
+		    input.table <- cbind(input.table, data.matrix[,i])
+		    input.table <- cbind(input.table, as.character(dataset$calls[,i]))
+	    }
+
+        row.names(input.table) <- row.names(data.matrix)
+        column.names <- c("ProbeID", column.names)
     }
-    else
-    {
-        dataset$calls
-    }
+
+    cat(column.names, file=combat.input.file.name, sep="\t")
+    cat("\n", file = combat.input.file.name, append=TRUE)
+    write.table(input.table, file=combat.input.file.name, sep="\t", col.names = FALSE, quote=FALSE, append=TRUE)
 
     if(prior.plots)
         pdf(file=paste(output.file.name, ".plot", sep=''), height = 12, width = 15)
 
-    combat.result <- ComBat(combat.input.file.name, sample.info.file.name, skip = 1, write = F, prior.plots = prior.plots, par.prior = par.prior)
+    combat.result <- ComBat(combat.input.file.name, sample.info.file.name, filter = filter, skip = 1, write = F, prior.plots = prior.plots, par.prior = par.prior)
 
     if(prior.plots)
         dev.off();
 
-    combat.result <- subset(combat.result, select=colnames(data.matrix))
-    row.names(combat.result) <- row.names(data.matrix)
+    gene.info <- combat.result[,1]
+    combat.result <- subset(combat.result, select = colnames(data.matrix))
 
-
+    row.names(combat.result) <- gene.info
     dataset$data <- combat.result
-    write.gct(dataset, output.file.name)
+
+    if(!is.null(dataset$row.descriptions))
+    {
+        descriptions.row <- rbind(row.names(data.matrix), dataset$row.descriptions)
+        descriptions.row <- descriptions.row[gene.info]
+        dataset$row.descriptions <- descriptions.row
+    }
+
+
+    if(!is.null(dataset$column.descriptions))
+    {
+        descriptions.column <- rbind(colnames(data.matrix), dataset$column.descriptions)
+        descriptions.column <- descriptions.column[colnames(combat.result)]
+        dataset$column.descriptions <- descriptions.column
+    }
+
+
+    if(!is.null(dataset$calls))
+    {
+        write.res(dataset, output.file.name)
+    }
+    else
+    {
+        write.gct(dataset, output.file.name)
+    }
 }
